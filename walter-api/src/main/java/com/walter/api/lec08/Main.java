2f307048f64c0131418730335973167c11e3c877
walter.hwang <lyvius2@naver.com>
2024-04-15 19:34:03 +0900

    [8강] JDK12 Text Block

---
+++ walter-api/src/main/java/com/walter/api/lec08/Main.java
@@ -0,0 +1,18 @@
+package com.walter.api.lec08;
+
+public class Main {
+	public static void main(String[] args) {
+		String textBlock = """
+			A
+			BC"
+			DEF
+			""";
+		System.out.println(textBlock);
+
+		String comment = """
+			Hello \
+			World!
+			""";
+		System.out.println(comment);
+	}
+}
