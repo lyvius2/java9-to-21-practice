ea753378c610a392f58bee2c695294dc87c15d78
walter.hwang <lyvius2@naver.com>
2024-04-16 22:56:07 +0900

    [10강] JDK16 Sealed Class

---
+++ walter-api/src/main/java/com/walter/api/lec10/sealed/Cat.java
@@ -0,0 +1,7 @@
+package com.walter.api.lec10.sealed;
+
+public final class Cat extends Animal {
+	public String purr() {
+		return "냐옹";
+	}
+}
---
+++ walter-api/src/main/java/com/walter/api/lec10/sealed/Dog.java
@@ -0,0 +1,7 @@
+package com.walter.api.lec10.sealed;
+
+public non-sealed class Dog extends Animal {
+	public String bark() {
+		return "으르렁컹컹";
+	}
+}
